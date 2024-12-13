db8633cd0616e6e5b14426dc782c5beab21fdaf4
rod-nickle <rod@bitwise-it.com>
2024-12-12 17:45:44 -0700

    Add Admin Dashboard counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,5 +4,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: username, password: password
 
   def show
+    @total_products = Product.count
+    @total_quantity = Product.sum("quantity")
+    @total_categories = Category.count
   end
 end
