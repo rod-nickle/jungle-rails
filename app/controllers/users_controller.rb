c5f08578b1e691254b351a45152d72711a161a7c
rod-nickle <rod@bitwise-it.com>
2024-12-18 17:22:00 -0700

    Show errors during User Registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,12 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
-      redirect_to :root
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to :root, notice: 'User created!'
     else
-      redirect_to :signup
+      render :new
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 8 }
+  validates :password, length: { minimum: 8 }
   validates :password_confirmation, presence: true
 
 
